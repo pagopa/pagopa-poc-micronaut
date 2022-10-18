93b7bda1288c341bd591ee8801894011c92e6921
AngeloCaporaso <angelocaporaso03@gmail.com>
2022-10-18 09:23:56 +0200

    config Swagger and Swagger UI for OpenApi Spec

--- src/main/java/it/gov/pagopa/reportingorgsenrollement/Application.java
+++ src/main/java/it/gov/pagopa/reportingorgsenrollement/Application.java
@@ -1,7 +1,20 @@
 package it.gov.pagopa.reportingorgsenrollement;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Reporting Organizations Enrollments",
+                version = "0.0.1",
+                description = "PagoPA API",
+                license = @License(name = "MIT"),
+                contact = @Contact(url = "https://www.pagopa.it/")
+        )
+)
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
