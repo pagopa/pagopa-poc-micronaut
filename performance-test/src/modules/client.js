0f5a18317df2c0b3b96385654b7f54d0af2b66f9
AngeloCaporaso <angelo.caporaso@pagopa.it>
2022-12-21 17:59:40 +0100

    feat (test): add GET organization performance test

--- performance-test/src/modules/client.js
+++ performance-test/src/modules/client.js
@@ -1,10 +1,28 @@
 import http from 'k6/http';
 
 
-export function example(rootUrl, params) {
-	const url = `${rootUrl}/`
+export function postOrganization(rootUrl, organizationFiscalCode) {
+	const url = `${rootUrl}/${organizationFiscalCode}`
 
-	const payload = {}
+	const params = {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    };
+
+	const payload = {
+	    "organizationFiscalCode": organizationFiscalCode
+	}
 
 	return http.post(url, JSON.stringify(payload), params);
+}
+
+export function deleteOrganization(rootUrl, organizationFiscalCode) {
+	const url = `${rootUrl}/${organizationFiscalCode}`
+	return http.del(url);
+}
+
+export function getOrganization(rootUrl, organizationFiscalCode) {
+	const url = `${rootUrl}/${organizationFiscalCode}`
+    return http.get(url);
 }
