e2ac18be4607b63b0dfab9d72eabb7fa40ce047c
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-01-26 15:49:36 +0100

    ci(integration test): add framework type param

--- integration-test/src/step_definitions/support/common.js
+++ integration-test/src/step_definitions/support/common.js
@@ -2,11 +2,12 @@ const axios = require("axios");
 const fs = require('fs');
 
 const reporting_enrollment_host = process.env.REPORTING_ENROLLMENT_HOST;
+const params = new URLSearchParams([['frameworkType', 'micronaut']]);
 
 function get(url) {
-    return axios.get(reporting_enrollment_host + url)
+    return axios.get(reporting_enrollment_host + url, {params})
          .then(res => {
-             return res;
+            return res;
          })
          .catch(error => {
              return error.response;
@@ -14,7 +15,7 @@ function get(url) {
 }
 
 function post(url, headers, body) {
-    return axios.post(reporting_enrollment_host + url, body, {headers})
+    return axios.post(reporting_enrollment_host + url, body, {params}, {headers})
         .then(res => {
             return res;
         })
@@ -24,7 +25,7 @@ function post(url, headers, body) {
 }
 
 function put(url, body) {
-    return axios.put(reporting_enrollment_host + url, body)
+    return axios.put(reporting_enrollment_host + url, body, {params})
         .then(res => {
             return res;
         })
@@ -35,7 +36,7 @@ function put(url, body) {
 
 
 function del(url) {
-    return axios.delete(reporting_enrollment_host + url)
+    return axios.delete(reporting_enrollment_host + url, {params})
         .then(res => {
             return res;
         })
